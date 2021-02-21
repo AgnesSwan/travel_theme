8d5dfaf29b464c137e47f12da2813160b740b630
Agnieszka Szmidtke <agnes.swan7@gmail.com>
2021-02-21 21:56:00 +0100

    first commit

---
+++ front-page.php
@@ -0,0 +1,21 @@
+
+<!--
+  motyw strony głównej
+
+  --->
+<!---wstrzykniecie pliku header.php--->
+<?php
+get_header(); 
+?>
+
+<main>
+<h1>
+elo
+</h1></main>
+
+<!---wstrzykniecie pliku footer.php get_footer();--->
+
+<?php
+get_footer();
+?>
+
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--
+  motyw strony głównej
+
+  --->
+<link rel ="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/style.css';?>">
+
+</head>
+<body>
+<header class="header_container">
+    <nav class="nav">
+      <div class="space"></div>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="#skills">About</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
